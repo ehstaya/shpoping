0cb8696e64935fe7038f1deeca9cadead2469701
Tariq Yaqub <ehstaya@gmail.com>
2018-11-17 22:01:36 -0800

    added

---
+++ shoping-tariq/src/main/java/com/tariq/spring/shoping/ShopingTariqApplication.java
@@ -0,0 +1,12 @@
+package com.tariq.spring.shoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingTariqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopingTariqApplication.class, args);
+	}
+}
---
+++ shoping-tariq/src/test/java/com/tariq/spring/shoping/ShopingTariqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tariq.spring.shoping;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopingTariqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
